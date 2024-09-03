363fe0026ce4eabb99043859c0b862fdf16542e9
MrElemental175 <jacobmasse2004@gmail.com>
2024-09-03 13:40:59 -0400

    Assignment 2 Finish

--- AssignmentTwo.py
+++ AssignmentTwo.py
@@ -1,12 +1,27 @@
+import math
 
 PackOfDogs = 10
 PackOfBuns = 8
 
-NumOfPeople = int(input("How many people are going to be at the cookout?"))
-NumOfDogs = int(input("How many Hotdogs will each person be given?"))
+NumOfPeople = float(input("How many people are going to be at the cookout?"))
+NumOfDogs = float(input("How many Hotdogs will each person be given?"))
 
 TotalDogs = NumOfDogs * NumOfPeople
+print("You need a total of ", TotalDogs, " hotdogs.")
 
 RequiredDogs = TotalDogs / PackOfDogs
 RequiredBuns = TotalDogs / PackOfBuns
 
+RemainderDogs = TotalDogs % PackOfDogs
+if RemainderDogs != 0:
+    RemainderDogs = PackOfDogs - RemainderDogs
+RemainderBuns = TotalDogs % PackOfBuns
+if RemainderBuns != 0:
+    RemainderBuns = PackOfBuns - RemainderBuns
+
+RequiredBuns = math.ceil(RequiredBuns)
+RequiredDogs = math.ceil(RequiredDogs)
+
+print("You will need ", RequiredDogs, "packs of hotdogs and ", RequiredBuns, "of hotdog buns.")
+print("You will have ", RemainderDogs, "hotdogs left.")
+print("You will have ", RemainderBuns, "hotdog buns left.")
