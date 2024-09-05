b36e872ba75310bb4cb29f4a2c7c93e032ff3e61
MrElemental175 <jacobmasse2004@gmail.com>
2024-09-05 11:40:30 -0400

    Updates to FileIO

--- InClassReview/fileIO.py
+++ InClassReview/fileIO.py
@@ -4,9 +4,15 @@ def main():
         students = studentList.readlines()
         studentList.close()
         #print(students)
+        fileOutput = open("output.txt, 'a")
         for s in students:
             #print(s)
             parts = s.split(',')
+            print(parts[0])
+            if parts[2] == "Math":
+                print(s)
+            fileOutput.write(parts[1] + "'s GPA is " + parts[3] + "\n")
+        fileOutput.close()
     
     except FileNotFoundError:
         print("File not found")
